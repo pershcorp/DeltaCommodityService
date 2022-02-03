d63fcb44cf89b3b5aa503fae57fb6b0f592701d9
DESKTOP-1LU89J4 <karthickpremoth@gmail.com>
2022-02-04 00:25:29 +0530

    chore: update swagger

--- src/main/java/com/pershcorporation/deltacommodityservice/controller/DefaultController.java
+++ src/main/java/com/pershcorporation/deltacommodityservice/controller/DefaultController.java
@@ -1,2 +1,21 @@
-package com.pershcorporation.deltacommodityservice.controller;public class DefaultController {
+package com.pershcorporation.deltacommodityservice.controller;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class DefaultController {
+
+    @GetMapping("/hello")
+    public ResponseEntity<Map<String, String>> introduction() {
+        Map<String, String> responseValue = new HashMap<>();
+        responseValue.put("swaggerURL", "http://localhost:8080/swagger-ui.html");
+        return ResponseEntity.ok(responseValue);
+    }
+
 }
